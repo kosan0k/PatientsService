d3c00c4b3acff92202efeb5fbdcaf6ea891c6f31
Alexander Zhishkevich <zhishkevich-am@adani.by>
2021-12-06 21:23:36 +0300

    +: controller

---
+++ PatientsService/Controllers/PatientsController.cs
@@ -0,0 +1,35 @@
+﻿using Hl7.Fhir.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using PatientsService.Models;
+using Task = System.Threading.Tasks.Task;
+
+namespace PatientsService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PatientsController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public PatientsController(ILogger<PatientsController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet("{id}")]
+        public Task<Response> Read(string id)
+        {
+            return Task.FromResult(new Response(HttpStatusCode.BadRequest));
+        }
+
+        [HttpPost]
+        public Task<Response> Create(Patient resource)
+        {
+            return Task.FromResult(new Response(HttpStatusCode.BadRequest));
+        }
+    }
+}
