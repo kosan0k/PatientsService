0d4c5558d24e258f16c5fc89fa636bc14a875942
Alexander Zhishkevich <zhishkevich-am@adani.by>
2021-12-06 21:23:15 +0300

    ~: simplify pfoject

---
+++ PatientsService/Models/Response.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using Hl7.Fhir.Model;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+
+namespace PatientsService.Models
+{
+    public class Response
+    {
+        public HttpStatusCode StatusCode;
+        public IKey Key;
+        public Resource Resource;
+
+        public Response(HttpStatusCode code, IKey key, Resource resource)
+        {
+            StatusCode = code;
+            Key = key;
+            Resource = resource;
+        }
+
+        public Response(HttpStatusCode code, Resource resource)
+        {
+            StatusCode = code;
+            Key = null;
+            Resource = resource;
+        }
+
+        public Response(HttpStatusCode code)
+        {
+            StatusCode = code;
+            Key = null;
+            Resource = null;
+        }
+
+        public bool IsValid
+        {
+            get
+            {
+                var code = (int)StatusCode;
+                return code <= 300;
+            }
+        }
+
+        public bool HasBody => Resource != null;
+
+        public override string ToString()
+        {
+            var details = (Resource != null) ? $"({Resource.TypeName})" : null;
+            var location = Key?.ToString();
+            return $"{(int) StatusCode}: {StatusCode} {details} ({location})";
+        }
+    }
+}
--- PatientsService/Startup.cs
+++ PatientsService/Startup.cs
@@ -2,11 +2,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PatientsService
 {
@@ -19,33 +14,18 @@ namespace PatientsService
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
-
-            app.UseStaticFiles();
-
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
